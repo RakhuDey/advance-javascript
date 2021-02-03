8aee084fad6a8fcef0b89d5aae533dd4e51d355a
RakhuDey <rakhu_dey@yahoo.com>
2021-02-03 16:46:40 +0600

    true vs false

---
+++ trueOrFalse.js
@@ -0,0 +1,20 @@
+// false list
+// 1=0
+// 2=""
+// 3=undefined
+// 4=null
+// 5=NaN
+// 6=false
+// true list
+// 1="0"
+// 2=" "
+// 3=[]
+// 4={}
+// 5=
+
+const ages = 4;
+if (ages > 0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
